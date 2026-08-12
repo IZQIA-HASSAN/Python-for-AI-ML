b47509e2093406af318a19780430d8607ba9e539
ls <izqiaulhassan166@gmail.com>
2026-08-12 21:52:40 +0500

    AI journey started on august 12, 2026

---
+++ test.py
@@ -0,0 +1,11 @@
+print("hello" , "wrold")
+
+my_age = 17
+my_name = "meerali"
+print(my_age , my_name)
+
+print(type(my_age))
+
+account_balance = 12
+
+print(isinstance(account_balance , (int , float)))
