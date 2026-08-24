cd1a8d663bb038e21a20da0c73fb45a9f304f533
ls <izqiaulhassan166@gmail.com>
2026-08-24 23:06:48 +0500

    function and scope LEGB done

--- test.py
+++ test.py
@@ -1,131 +1,188 @@
-# print("hello" , "wrold")
+# # print("hello" , "wrold")
 
-# my_age = 17
-# my_name = "meerali"
-# print(my_age , my_name)
+# # my_age = 17
+# # my_name = "meerali"
+# # print(my_age , my_name)
 
-# print(type(my_age))
+# # print(type(my_age))
 
-# account_balance = 12
+# # account_balance = 12
 
-# print(isinstance(account_balance , (int , float)))
+# # print(isinstance(account_balance , (int , float)))
 
-# # strings 
+# # # strings 
 
-# name  = "hassan"
+# # name  = "hassan"
 
-# namer  = '''my name is hasan 
-# and i am learning python '''
+# # namer  = '''my name is hasan 
+# # and i am learning python '''
 
-# print(name , namer)
+# # print(name , namer)
 
-# quote = "quaid said 'work work and stop'"
-# print(quote)
+# # quote = "quaid said 'work work and stop'"
+# # print(quote)
 
-# msg= "it's a sunny day"
-# msg2 = "she said , \"hello!\""
-# print(msg , msg2)\
+# # msg= "it's a sunny day"
+# # msg2 = "she said , \"hello!\""
+# # print(msg , msg2)\
 
-# print("sunny" in msg)
-
-
-# mystring = "muhammad"
-# print(len(mystring))
-
-# print(mystring[-1])
-
-# sound = "ha"
-# anothersound = 3
-# fullsound = sound * anothersound
-# print(fullsound)
-
-# str1 = "izqia"
-# str2 = "hassan"
-# str4 = 23
-# str3 = str1 + str2
-# str5 = str1 + str4 
-# print(str5)
-# print(str3)
-
-name = "johndoe"
-age  = 18
-# fullname = "my name is " + name + " and my age is " + str(age)
-# print(fullname) 
-
-name_and_age = name
-name_and_age += str(age)
-
-print(name_and_age)
-
-interpolation = f'my nameis {name} and my age is {age} , good bye'
-print(interpolation)
-
-# slice in pytohn 
-my_str = "Hello world"
-print(my_str[2:-1])
-print(my_str[:-1])
-print(my_str[8:])
-print(my_str[:])
-print(my_str[0:8:2])
-print(my_str[::-1])
-
-# some common string methods
-
-school = "post graduate college mansehra "
-
-uppercase = school.upper()
-lower = school.lower()
-print(uppercase)
-print(lower)
-
-striping = school.strip()
-# works same as trim in javascript
-print(striping)  
-
-replaced = school.replace("post" , "mansehra")
-print(replaced)
-
-spliting = school.split()
-print(spliting)
-
-mylist = ["table" , "tennis"]
-joined = ' '.join(mylist)
-print(joined)
-# print(mylist.join(mylist))
-
-start = school.startswith(" post")
-print(start)
-
-ends = school.endswith(" ")
-print(ends)
-
-finder = school.find("mansehra")
-print(finder)
-
-counter = school.count("a")
-print(counter)
-
-cap = school.capitalize()
-print(cap)
-
-upp = school.isupper()
-print(upp)
-
-low = school.islower()
-print(low)
-
-capital = school.title()
-print(capital)
+# # print("sunny" in msg)
 
 
-age  = 13
-name = "ahmed khan"
+# # mystring = "muhammad"
+# # print(len(mystring))
 
-if age > 13 :
-    print("age is not verified")
-elif age <= 13:
-        print("age is eligible")
-else:
-        print("okay they are working")
+# # print(mystring[-1])
+
+# # sound = "ha"
+# # anothersound = 3
+# # fullsound = sound * anothersound
+# # print(fullsound)
+
+# # str1 = "izqia"
+# # str2 = "hassan"
+# # str4 = 23
+# # str3 = str1 + str2
+# # str5 = str1 + str4 
+# # print(str5)
+# # print(str3)
+
+# name = "johndoe"
+# age  = 18
+# # fullname = "my name is " + name + " and my age is " + str(age)
+# # print(fullname) 
+
+# name_and_age = name
+# name_and_age += str(age)
+
+# print(name_and_age)
+
+# interpolation = f'my nameis {name} and my age is {age} , good bye'
+# print(interpolation)
+
+# # slice in pytohn 
+# my_str = "Hello world"
+# print(my_str[2:-1])
+# print(my_str[:-1])
+# print(my_str[8:])
+# print(my_str[:])
+# print(my_str[0:8:2])
+# print(my_str[::-1])
+
+# # some common string methods
+
+# school = "post graduate college mansehra "
+
+# uppercase = school.upper()
+# lower = school.lower()
+# # print(uppercase)
+# print(lower)
+
+# striping = school.strip()
+# # works same as trim in javascript
+# print(striping)  
+
+# replaced = school.replace("post" , "mansehra")
+# print(replaced)
+
+# spliting = school.split()
+# print(spliting)
+
+# mylist = ["table" , "tennis"]
+# joined = ' '.join(mylist)
+# print(joined)
+# # print(mylist.join(mylist))
+
+# start = school.startswith(" post")
+# # print(start)
+
+# ends = school.endswith(" ")
+# print(ends)
+
+# finder = school.find("mansehra")
+# print(finder)
+
+# counter = school.count("a")
+# print(counter)
+
+# cap = school.capitalize()
+# print(cap)
+
+# upp = school.isupper()
+# print(upp)
+
+# low = school.islower()
+# print(low)
+
+# capital = school.title()
+# print(capital)
+
+
+# age  = 13
+# name = "ahmed khan"
+
+# if age > 13 :
+#     print("age is not verified")
+# elif age <= 13:
+#         print("age is eligible")
+# else:
+#         print("okay they are working")
 
 # identation matters alot in python
+
+# def sum (a, b):
+#     return a +  b 
+
+# sum = sum(3, 4)
+# print(sum)    
+
+# local scope
+# def my_fun():
+#     my_var = 10
+#     print(my_var)
+
+# my_fun()
+
+# print(my-var)
+
+# Enclosing scope
+
+# def outer_fun():
+#     msg = "outer function"
+#     res = ""
+#     def inner_fun():
+#         nonlocal res
+#         res = "this is inner non-local variable"
+#         print(msg)
+
+#     inner_fun()
+#     print(res)
+
+# outer_fun()
+
+
+
+num = 10
+
+# def fun():
+#     print("this is inner fun" , num)
+
+#     def fun2():
+#         print("this is inner2" , num)
+
+#     fun2()
+
+#     # fun2()
+
+# fun()    
+
+
+my_var = 10  # A global variable
+
+def change_var():
+    global my_var  # Allows modification of a global variable
+    my_var = 20
+
+change_var()
+
+print(my_var)
